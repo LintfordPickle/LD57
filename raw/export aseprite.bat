22fa8052aa91f22647f0ba30f6af77d3fe39dc86
LintfordPickle <jonnohampson@gmail.com>
2024-04-15 21:17:24 +0200

    added settlement animations

--- raw/export aseprite.bat
+++ raw/export aseprite.bat
@@ -2,5 +2,6 @@ set location=%~dp0
 
 aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\attack.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\attack00.png"
 aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\flag.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\flag00.png"
+aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\pentagram.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\pentagram00.png"
 
 pause
