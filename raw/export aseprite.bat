54ac7c9485024002fd45513653764520f828cd19
LintfordPickle <jonnohampson@gmail.com>
2025-03-21 22:19:18 +0100

    cleaned up sample resources (raw)

--- raw/export aseprite.bat
+++ raw/export aseprite.bat
@@ -1,7 +1,5 @@
 set location=%~dp0
 
-aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\attack.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\attack00.png"
-aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\flag.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\flag00.png"
-aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\pentagram.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\pentagram00.png"
+REM aseprite -b --ignore-empty "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\aseprite\attack.aseprite" --save-as "D:\dev\java\ludumdare\55 - Summoning\projects\FanTac\raw\spritesheets\game\attack00.png"
 
 pause
